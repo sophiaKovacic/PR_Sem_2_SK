d0a768fbfecb94ca58b9f0a0c5cc2285c7feb270
Sophia <sophia.kovacic@gmail.com>
2020-05-20 02:26:58 +0200

    Create fcn.c

---
+++ Hausaufgabe_ComplexNumbers/fcn.c
@@ -0,0 +1,30 @@
+//NAME: Sophia Kovacic
+//DATUM: 18.05.2020
+//AUFGABE: Taschenrechner komplexe Zahlen
+
+
+
+
+#include <stdio.h>
+#pragma warning(disable:4996)
+#include <math.h>
+
+
+
+
+double fcnadd(double num1, double num2) {
+
+	double erg = num1 + num2;
+	return erg;
+
+}
+
+
+
+double fcnsub(double num1, double num2) {
+
+	double erg = num1 - num2;
+	return erg;
+
+
+}
