881c8334b1c4628baa04cb874da5a508a8be1162
Sophia <sophia.kovacic@gmail.com>
2020-05-20 02:26:08 +0200

    Create fcn.h

---
+++ Hausaufgabe_ComplexNumbers/fcn.h
@@ -0,0 +1,14 @@
+//NAME: Sophia Kovacic
+//DATUM: 18.05.2020
+//AUFGABE: Taschenrechner komplexe Zahlen
+
+
+
+#ifndef Header_h_
+#define Header_h_
+
+
+double fcnadd(double, double);
+double fcnsub(double, double);
+
+#endif
