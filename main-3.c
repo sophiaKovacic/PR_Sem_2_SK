84c23180e12399882854d8c274b20526becfbd68
Sophia <sophia.kovacic@gmail.com>
2020-05-27 00:57:15 +0200

    Update main-3.c

--- main-3.c
+++ main-3.c
@@ -1,114 +1,45 @@
-// NAME: Sophia Kovacic
-//AUFGABE: Newton-Raphson-Verfahren
-//Datum: 13.05.2020
-
-
-
-
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
-
-//Koennen auch in .h Datei bis Zeile 33 (Global)
-
-
-
-double func(double x) {                                                     //double gew�hlt zwecks Genauigkeit
-                                                                            //Werte in Funktion eingesetzt
-    int a = 2;
-    int b = -6;
-    int c = 9;
-
-    return (pow(x, a) + b * x + c);
-}
-
-double der(double x) {                                                      //double gew�hlt zwecks genauigkeit
-                                                                            //Werte in Ableitung eingesetzt
-
-    int a = 2;
-    int b = -6;
-    int c = 9;
-
-    return (a * x) + b;
-}
+#include <string.h>
+#include "HA03_linkedListLib.h"
 
 
-int main() {
+int main(){
 
-    int a = 2; 
-    int b = -6;
-    int c = 9;
-
-    //printf("Geben Sie a,b und c ein (x^a + bx + c ) : \n");
-    //scanf("%d.%d.%d",&a,&b,&c);
-   // printf("Ihre Eingabe war: %d, %d, %d, \n",a,b,c);
-
-
-
-    float tol = 0.00000001;
-    double x0 = 5, x1 = 0, epsilon = 1.0E-10;                              //x0 = 5 ist der Anfangswert
-    int max_wiederholungen;
-
-    double* values = malloc(20 * sizeof(*values));                            //zugeordneter Speicher
-    if (values == NULL) {
-        printf("Platz im Speicher reicht nicht aus.\n Das Programm wird beendet...\n");
-        return 0;
-    }
-
-    printf("Geben Sie die Anzahl der Wiederholungen an ( < 20) \n");
-    scanf("%d", &max_wiederholungen);
-    if (max_wiederholungen >= 20) {
-        printf("Eingabe war h�her als 20.\nErneute Eingabe (unter 20): ");
-        scanf("%d", &max_wiederholungen);
-        if (max_wiederholungen >= 20);
-        printf("Eingabe war wieder >= 20. Wird nun beendet.\n");
-        return 0;
-    }
-
-    printf("Wiederholung\t x0\t\t x1\t\t func\t\t der\n");
-    printf("---------\t -\t\t -\t\t -----\t\t --------\n");
-
-    //Newton-Raphson-Verfahren
-    for (int i = 0;i <= max_wiederholungen;i++) {
-        x1 = x0 - func(x0) / der(x0);                                         //Rechnung des Verfahrens
-        printf("%d\t\t %.8f\t %.8f\t %.8f\t %.8f\n", i, x0, x1, func(x0), der(x0));  // Ausgabe
-        if (fabs(der(x0)) < epsilon) {
-            printf("Die Zahl ist zum dividieren zu klein");             //durch 0 ist nicht teilbar
-            return 0;
+    listElement *start;
+    start = (listElement *)malloc(sizeof(listElement));
+    if (start == NULL) {
+        printf("can't reserve storage.\n");
+        return -1;
         }
-        else {
-            x0 = x1;
-            values[i] = x0;                                             //Gibt das entwickelte ergebnis zu dem array zur Kontrolle der Genauigkeit
+    start->nextElem = NULL;
+
+    int FLAGG = 1;
+    while (FLAGG){
+    
+        printf("1... print list\n");
+        printf("2... add element\n");
+        printf("3... delete element\n");
+        printf("4... delete list\n");
+        printf("5... save list\n");
+        printf("6... load list\n");
+        printf("7... sort list\n");
+        printf("0... quit\n");
+        scanf("%d",&FLAGG);
+        system("clear"); // cls for windows
+
+
+        switch (FLAGG){
+            case 1: printList(start); break;
+            case 2: addListElem(start); break;
+            case 3: delListElem(start); break;
+            case 4: delList(start); break;
+            case 5: saveList(start); break;
+            case 6: loadList(start); break;
+            case 7: sortList(start); break;
+            case 0: FLAGG = 0; exitFcn(start); break;
+            default: printf("invalid choice\n"); break;
         }
     }
-
-    if (fabs(values[max_wiederholungen] - values[max_wiederholungen - 1]) <= tol) { //wie pr�zise arbeitet das Programm
-        printf("Das Ergebnis nach der %d - ten Wiederholung, ist: %.10f.\n", max_wiederholungen, x1);
-        printf("%d -te Wiederholung ist: %.8f\n", max_wiederholungen, values[max_wiederholungen]);
-        printf("%d -te Wiederholung ist: %.8f\n", max_wiederholungen - 1, values[max_wiederholungen - 1]);
-        printf("Der m�gliche Fehler in der Sch�tzung liegt bei %.8f\n", fabs(values[max_wiederholungen] - values[max_wiederholungen - 1]));
-        printf("wo unsere Toleranz bei %.8f liegt", tol);
-    }
-    else {
-
-        printf("%d -te Wiederholung ist: %.8f\n", max_wiederholungen, values[max_wiederholungen]);
-        printf("%d -te Wiederholung ist: %.8f\n", max_wiederholungen - 1, values[max_wiederholungen - 1]);
-        printf("Der m�gliche Fehler in der Sch�tzung betr�gt:  %.8f\n", fabs(values[max_wiederholungen] - values[max_wiederholungen - 1]));
-        printf("unsere Toleranz liegt bei %.8f ", tol);
-    }
-
-    free(values); //leert den freien Speicherplatz
-
-
-    return (0);
+    return 0;
 }
-
-/*
-JW-2020-05-12:
---------------
-> very good implementation!! (9/10) 
-> missing extra lib (*.c + *.h) incl. includegard includeguards: -1 Pkt
-> a,b,c in main are unused
-> Please check row 63. If-statement has empty body. Program returns immediately if user enters value > 20. Even if second input is < 20 
-*/
-
