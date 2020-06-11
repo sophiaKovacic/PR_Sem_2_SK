3faae61f2d92ff63bf5136c0a0d1450789a8a4f9
Sophia <sophia.kovacic@gmail.com>
2020-06-11 22:02:17 +0200

    Update HA05_rhsLib.c

--- Hausaufgaben_05/HA05_rhsLib.c
+++ Hausaufgaben_05/HA05_rhsLib.c
@@ -10,54 +10,37 @@ void RHS_MSD(double* rhs, double* y) {
     double c = 5.0; // spring constant
     double d = 1.25; // damper constant
 
-
-
     double x = y[0]; // position
     double v = y[1]; // speed
+    
+    double x_dot = -((d / m) * v + (c / m) * x);
+    double y_dot = v;
 
 
 
-    rhs[0] = v;
-    rhs[1] = -((d / m) * v + (c / m) * x);
+    rhs[0] = y_dot
+    rhs[1] = x_dot
 
 }
 
-
-
 void myRHS_Ball(double* rhs, double* y) {
 
-
-
     double x = y[0]; // position
-
     double v = y[1]; // speed
 
-
-
     double m = 1.0; // mass of object
-
     double g = 9.81; // gravity constant
-
     double c = 5.0; // feder constant
-
     double rhoL = 1; // density of air
-
     double cW = 3; // objects cW value
-
-    double A = 2.5; // objects area 
-
+    double A = 2.5; // objects area
 
 
     double forceSign = 1; // sign for air resistance (drag)
-
     if (v > 0) forceSign = -1;
 
 
-
     rhs[0] = v;
-
     rhs[1] = -(g + (c / m) * x) + 1.0 / (2.0 * m) * rhoL * cW * A * v * v * forceSign;
-
-
 
 }
